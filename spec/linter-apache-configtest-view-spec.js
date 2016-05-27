e8779f4c8ce33035e5cbd2c6139a926375550c2e
Jacob Champion <champion.p@gmail.com>
2016-05-27 12:26:22 -0700

    Create a linter plugin
    - Remove unneeded code (styles, keymaps, menus)
    - Add npm package dependencies on atom-linter and atom-package-deps
    - Add apm package dependency on linter
    - Plug into the linter API with provideLinter()
    - Provide a very simple linter implementation (which currently only
      displays a warning for every configuration)

--- spec/linter-apache-configtest-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LinterApacheConfigtestView from '../lib/linter-apache-configtest-view';
-
-describe('LinterApacheConfigtestView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
